b6e5699aed3d410ac68ec13cefb34bad81f3c74e
Edita Pesa <editapesa@gmail.com>
2021-04-19 20:35:46 -0400

    add routes to server js file

--- Develop/server.js
+++ Develop/server.js
@@ -7,5 +7,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => console.log('Express server listening'));
