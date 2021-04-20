fc4f76e87c17ab65a319cfe07abdec9e9caca71a
Edita Pesa <editapesa@gmail.com>
2021-04-19 20:27:15 -0400

    add routes, and store js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
-app.get('/api/notes', (req, res) => res.json(notes));
 
 app.listen(PORT, () => console.log('Express server listening'));
