e0e36034170d4b519b5f81d1b08a2d1a793a3686
Edita Pesa <editapesa@gmail.com>
2021-04-20 23:29:03 -0400

    chng store js, remove util

--- db/store.js
+++ db/store.js
@@ -1,17 +1,12 @@
-const util = require('util');
 const fs = require('fs');
-
 const generateUniqueId = require('../node_modules/generate-unique-id');
-
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
     read() {
-        return readFileAsync('db/db.json', 'utf8');
+        return fs.readFile('db/db.json');
     }
     write(note) {
-        return writeFileAsync('db/db.json', JSON.stringify(note));
+        return fs.writeFile('db/db.json', JSON.stringify(note));
     }
     getNotes() {
         return (this.read())
